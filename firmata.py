9a37b04d96ea8b9736e56718a9537772de807837
Muzammal <muzammal0@gmail.com>
2022-07-18 13:11:01 +0500

    firmata

---
+++ firmata.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import pyfirmata
+import time
+if __name__ == '__main__':
+    # Initiate communication with Arduino
+    board = pyfirmata.Arduino('YOUR_PORT_HERE')
+    print("Communication Successfully started")
+    while True:
+        board.digital[13].write(1)
+        time.sleep(1)
+        board.digital[13].write(0)
+        time.sleep(1)
